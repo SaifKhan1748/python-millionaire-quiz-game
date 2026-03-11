c5bdc4723bc1df240d41f3a51b128dbc6bb98747
saif <saifkhan1748@gmail.com>
2026-03-11 16:16:33 +0530

    added more questions

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ questions=[
         ["What is the capital of India?","New Delhi","Mumbai","Chennai","Kolkata",1],
         ["What is the currency of japan?","dollar","euro","peso","yen",4],
         ["What is the capital of USA?","Washington Dc","New York","Los Angeles","Chicago",1],
-        ["What is the capital of UK?","Sarigamo","London","Birmingham","Liverpool",2]
-        ]
+        ["What is the capital of UK?","Sarigamo","London","Birmingham","Liverpool",2],
+        ["Who is known as the Father of India?","Subhash Chandra Bose","Mahatma Gandhi","Bhagat Singh","Jawaharlal Nehru",2],
+        ["Which planet is known as the Red Planet?","Earth","Mars","Venus","Jupiter",2],
+        ["Which language is used for AI and Machine Learning most commonly?","Python","HTML","CSS","C",1],
+        ["What is 5 + 7?","10","11","12","13",3],
+        ["Which is the largest ocean in the world?","Atlantic Ocean","Indian Ocean","Arctic Ocean","Pacific Ocean",4],
+        ["Which animal is known as the King of the Jungle?","Tiger","Lion","Elephant","Leopard",2],
+        ["How many continents are there in the world?","5","6","7","8",3],
+        ["Which is the fastest land animal?","Cheetah","Lion","Horse","Tiger",1],
+        ["Which country invented Pizza?","France","Italy","USA","Spain",2],
+        ["Which device is used to input text into a computer?","Monitor","Keyboard","Printer","Speaker",2]
+] 
+
 
 prize_money=[1000,3000,5000,10000,50000]
 i=0
